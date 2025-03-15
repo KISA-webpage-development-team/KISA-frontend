698f510ac963db6ceafba097684b8acd3cc7336c
retz8 <ekkicb71@gmail.com>
2025-03-15 17:13:05 -0400

    add react-navigation setup

--- apps/mobile/pocha/src/screens/CartScreen.tsx
+++ apps/mobile/pocha/src/screens/CartScreen.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import React from 'react';
+
 // UI
 import PochaBackHeading from '@/shared/components/feedback/PochaBackHeading';
 import EmptyCartAlert from '@/components/cart/EmptyCartAlert';
---
+++ apps/mobile/pocha/src/screens/PaySuccessScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export default function PaySuccessScreen() {
+  return (
+    <View>
+      <Text>PaySuccessScreen</Text>
+    </View>
+  );
+}
