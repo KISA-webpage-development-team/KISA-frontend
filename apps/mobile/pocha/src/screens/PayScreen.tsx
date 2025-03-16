368a76ea00d0a1e61721b575f67392dff21b7ee8
retz8 <ekkicb71@gmail.com>
2025-03-16 00:13:22 -0400

    add template for PayScreen

--- apps/mobile/pocha/src/screens/PayScreen.tsx
+++ apps/mobile/pocha/src/screens/PayScreen.tsx
@@ -1,10 +1,14 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
+import PaySummaryCard from '@/components/pay/PaySummaryCard';
+import PayButton from '@/components/pay/PayButton';
 
 export default function PayScreen() {
   return (
     <View>
-      <Text>PayScreen</Text>
+      {/* [TODO] PaymentSubmitForm will go here... */}
+      <PaySummaryCard />
+      <PayButton />
     </View>
   );
 }
