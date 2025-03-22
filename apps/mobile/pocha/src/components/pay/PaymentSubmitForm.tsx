bbd9d66765923f677bc6160af1b22228ae5af7d1
ysna99 <110143284+ysna99@users.noreply.github.com>
2025-03-22 15:42:28 -0400

    adjh

--- apps/mobile/pocha/src/components/pay/PaymentSubmitForm.tsx
+++ apps/mobile/pocha/src/components/pay/PaymentSubmitForm.tsx
@@ -10,7 +10,7 @@
 
 import React, {useState} from 'react';
 import { CardField, useStripe } from '@stripe/stripe-react-native';
-import {View, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 // import convertToSubcurrency from '@/lib/stripe/convertToSubcurrency';
 //import {useRouter} from 'next/navigation';
 // import {sejongHospitalBold} from '@/utils/fonts/textFonts';
@@ -115,9 +115,7 @@ export default function PaymentSubmitForm({
         }}
         cardStyle={cardFieldStyles}
         style={styles.cardContainer}
-        onCardChange={cardDetails => {
-
-        }}
+        //onCardChange={cardDetails => { }}
       />
       {/* )} */}
       {/*?*/}
