b400561e8f953d58bdea5629aad614cb964d1cf9
retz8 <ekkicb71@gmail.com>
2025-04-07 23:45:44 -0400

    build

--- apps/mobile/pocha/src/shared/lib/axios/client.ts
+++ apps/mobile/pocha/src/shared/lib/axios/client.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 import {BACKEND_URL, LOCAL_BACKEND_URL, USE_LOCAL_BACKEND} from '@env';
 
 const client = axios.create({
-  // baseURL: USE_LOCAL_BACKEND ? LOCAL_BACKEND_URL : BACKEND_URL,
-  baseURL: 'https://6fb0-67-194-25-84.ngrok-free.app/api/v2',
+  baseURL: BACKEND_URL,
+  // baseURL: 'https://6fb0-67-194-25-84.ngrok-free.app/api/v2',
   headers: {
     'Content-Type': 'application/json',
   },
